3c449148c6fa1ddf81411c3d8fb493db3b343fd3
Sabina Mussaeva <smussaeva@ozon.ru>
2022-04-11 02:26:07 +0300

    with 'lastFailureUnfixedDays'

--- TRT_microservice/results/views.py
+++ TRT_microservice/results/views.py
@@ -16,19 +16,20 @@ class AddResultView(generics.CreateAPIView):
 class GetResultsView(APIView):
 
     # http://127.0.0.1:8000/testResults?squad=testtest&daysToFollow=2
-    def get(self, request):
+    def get(self):
         squad_to_get = self.request.query_params.get('squad')
         days_to_get = int(self.request.query_params.get('daysToFollow'))
-        needed_date = datetime.datetime.now() - datetime.timedelta(days=days_to_get)
+        today = datetime.date.today()
+        needed_date = today - datetime.timedelta(days=days_to_get)
 
         db_data = TestResults.objects\
             .values('test_date')\
             .annotate(noffailures=Sum('failed_tests'))\
             .filter(squad__iexact=squad_to_get, test_date__gte=needed_date)
-        # SELECT date, sum(failed_tests) from table GROUP BY date HAVING squad = <squad> AND date > <needed>
+        # SELECT date, sum(failed_tests) from table GROUP BY date HAVING squad = <squad_to_get> AND date > <needed_date>
 
         details = {
-            str(obj["test_date"]): {"status": "passed" if obj["noffailures"] else "failed",
+            str(obj["test_date"]): {"status": "passed" if not obj["noffailures"] else "failed",
                                     "noffailures": obj["noffailures"]}
             for obj in db_data
         }
@@ -36,10 +37,16 @@ class GetResultsView(APIView):
         totalDaysfailed = sum((1 if test_data["noffailures"] else 0 for test_data in details.values()))
         verdict = "stable" if 1 < totalDaysfailed / totalDays < 0.10 else "unstable"
         lastFailureUnfixedDays = 0
-        data = {
-            "details": details,
-            "totalDaysfailed": totalDaysfailed,
-            "verdict": verdict,
-            "lastFailureUnfixedDays": lastFailureUnfixedDays
-        }
-        return Response(data)
+
+        if db_data.order_by('-test_date')[:1][0]["noffailures"] != 0:
+            last_failure_day = db_data.values('test_date').filter(failed_tests__gte=0).order_by('-test_date')[:1][0]
+            lastFailureUnfixedDays = (today - last_failure_day['test_date']).days
+
+        return Response(
+            {
+                "details": details,
+                "totalDaysfailed": totalDaysfailed,
+                "verdict": verdict,
+                "lastFailureUnfixedDays": lastFailureUnfixedDays
+            }
+        )
