e307fcf9156b8845eb71e0a275cb90e6eea96598
Sabina Mussaeva <smussaeva@ozon.ru>
2022-04-11 16:48:58 +0300

    added exception

--- TRT_microservice/TRT_microservice/settings.py
+++ TRT_microservice/TRT_microservice/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(8jem%h-5-(7bir^xv+*4$adm7557w)#i3ru63$f-5v9enaiee'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- TRT_microservice/results/views.py
+++ TRT_microservice/results/views.py
@@ -1,5 +1,6 @@
 import datetime
 
+from django.core.exceptions import BadRequest
 from django.db.models import Sum
 from rest_framework import generics
 from rest_framework.response import Response
@@ -17,36 +18,39 @@ class GetResultsView(APIView):
 
     # http://127.0.0.1:8000/testResults?squad=testtest&daysToFollow=2
     def get(self, request):
-        squad_to_get = request.query_params.get('squad')
-        days_to_get = int(request.query_params.get('daysToFollow'))
-        today = datetime.date.today()
-        needed_date = today - datetime.timedelta(days=days_to_get)
+        try:
+            squad_to_get = request.query_params.get('squad')
+            days_to_get = int(request.query_params.get('daysToFollow'))
+            today = datetime.date.today()
+            needed_date = today - datetime.timedelta(days=days_to_get)
 
-        db_data = TestResults.objects\
-            .values('test_date')\
-            .annotate(noffailures=Sum('failed_tests'))\
-            .filter(squad__iexact=squad_to_get, test_date__gte=needed_date)
-        # SELECT date, sum(failed_tests) from table GROUP BY date HAVING squad = <squad_to_get> AND date > <needed_date>
+            db_data = TestResults.objects\
+                .values('test_date')\
+                .annotate(noffailures=Sum('failed_tests'))\
+                .filter(squad__iexact=squad_to_get, test_date__gte=needed_date)
+            # SELECT date, sum(failed_tests) from table GROUP BY date HAVING squad = <squad_to_get> AND date > <needed_date>
 
-        details = {
-            str(obj["test_date"]): {"status": "passed" if not obj["noffailures"] else "failed",
-                                    "noffailures": obj["noffailures"]}
-            for obj in db_data
-        }
+            details = {
+                str(obj["test_date"]): {"status": "passed" if not obj["noffailures"] else "failed",
+                                        "noffailures": obj["noffailures"]}
+                for obj in db_data
+            }
 
-        totalDays = sum((1 for _ in details))
-        totalDaysfailed = sum((1 if test_data["noffailures"] else 0 for test_data in details.values()))
-        verdict = "stable" if 1 < totalDaysfailed / totalDays < 0.10 else "unstable"
+            totalDays = sum((1 for _ in details))
+            totalDaysfailed = sum((1 if test_data["noffailures"] else 0 for test_data in details.values()))
+            verdict = "stable" if 1 < totalDaysfailed / totalDays < 0.10 else "unstable"
 
-        lastFailureUnfixedDays = 0
-        if len(db_data.filter(noffailures=0)) and db_data.order_by('-test_date')[:1][0]["noffailures"] != 0:  # if we have even 1 date passed
-            last_failures = []
-            for obj in db_data.order_by('-test_date'):
-                if obj['noffailures'] > 0:
-                    last_failures.append(obj['test_date'])
-                else:
-                    break
-            lastFailureUnfixedDays = (today - last_failures.sort()[0]).days
+            lastFailureUnfixedDays = 0
+            if len(db_data.filter(noffailures=0)) and db_data.order_by('-test_date')[:1][0]["noffailures"] != 0:  # if we have even 1 date passed
+                last_failures = []
+                for obj in db_data.order_by('-test_date'):
+                    if obj['noffailures'] > 0:
+                        last_failures.append(obj['test_date'])
+                    else:
+                        break
+                lastFailureUnfixedDays = (today - last_failures.sort()[0]).days
+        except Exception:
+            raise BadRequest("Invalid request. Please enter required parameters: 'squad', 'daysToFollow'")
 
         return Response(
             {
